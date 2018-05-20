6a5859a42455b16e21b1c3d48e485da2eb7c461a
navdeyev <navdeyev@gmail.com>
2018-05-20 10:01:06 +0100

    Loading user list from external source and displaying it.

---
+++ src/components/UserList/UserList.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import {FlatList, ListRenderItemInfo, StyleSheet, Text} from 'react-native';
+
+import {User} from '../../domains/types';
+
+interface Props {
+}
+
+interface State {
+    userData: Array<User>
+}
+
+class UserList extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            userData: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => this.setState(() => {
+                return {userData: json};
+            }));
+    }
+
+    renderItem = (info: ListRenderItemInfo<User>) => {
+        const user = info.item;
+        return <Text style={styles.userRow}>{user.name} : {user.email}</Text>;
+    };
+
+    render() {
+        return (
+            <FlatList
+                style={styles.userList}
+                data={this.state.userData}
+                renderItem={this.renderItem}
+                keyExtractor={(user: User) => '' + user.id}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    userList: {
+        marginTop: 25,
+    },
+    userRow: {
+        padding: 10
+    }
+});
+
+export default UserList;
